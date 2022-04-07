6b9a384f23e16dd249e5ddb365fbc154a5e302fc
Jeff Epler <jepler@gmail.com>
2022-04-07 18:53:30 -0500

    Add --linear/--polar
    --linear ignores --diameter

--- fluxvis/__main__.py
+++ fluxvis/__main__.py
@@ -13,14 +13,19 @@ from greaseweazle.image.scp import SCP
 @click.option("--start", default=0, help="Number of the first tracks (default: 0)")
 @click.option("--side", default=0, help="Side of floppy (default: 0)")
 @click.option("--resolution", default=960, help="Resolution of square output image (default: 960)")
+@click.option("--linear/--polar", default=False, help="Use a linear instead of polar image")
 @click.argument("input", type=click.Path(exists=True))
 @click.argument("output", type=click.Path())
-def main(slices, stacks, diameter, tracks, input, output, stride, start, side, resolution):
+def main(slices, stacks, diameter, tracks, input, output, stride, start, side, resolution, linear):
     """Visualize INPUT (an scp-format flux file) to OUTPUT (a png file)
     """
     flux = SCP.from_file(input)
 
-    major = round(diameter*stacks/2)
+    if linear:
+        major = round(tracks*stacks)
+    else:
+        major = round(diameter*stacks/2)
+
     density = np.zeros((major , slices), dtype=np.float32)
     r = np.arange(slices)
 
@@ -29,11 +34,15 @@ def main(slices, stacks, diameter, tracks, input, output, stride, start, side, r
         t1 = major - cyl * stacks - 1
         track = flux.get_track(cyl*stride+start, side)
         if track is None: continue
+        print(track.index_list)
         track.cue_at_index()
+        print(track.index_list)
+        print(flux.sample_freq)
         if not track.index_list:
             index = sum(track.list)
         else:
             index = track.index_list[0]
+        print(index / flux.sample_freq)
         index = track.index_list[0]
         indices = np.array(track.list, dtype=np.float32) * (slices/index)
         indices = np.floor(np.add.accumulate(indices))
@@ -41,10 +50,11 @@ def main(slices, stacks, diameter, tracks, input, output, stride, start, side, r
         hi = np.searchsorted(indices, r, 'right')
         density[t0:t1, :] = hi - lo
 
-    wrapped = invpolar.warp_inverse_polar(density, output_shape=(resolution, resolution))
-    maxdensity = np.max(wrapped)
-
-    imsave(output, (wrapped * (255/maxdensity)).astype(np.uint8))
+    if not linear:
+        density = invpolar.warp_inverse_polar(density, output_shape=(resolution, resolution))
+    maxdensity = np.max(density)
+    
+    imsave(output, (density * (255/maxdensity)).astype(np.uint8))
 
 if __name__ == '__main__':
     main()
