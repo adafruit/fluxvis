8abe172f27d44d7bc18bdf4afa4a96163d07ad41
Jeff Epler <jepler@gmail.com>
2022-04-10 09:35:09 -0500

    Update docs & image generator

--- etc/update-img.sh
+++ etc/update-img.sh
@@ -3,4 +3,4 @@ c1541 -format example,ds d64 disk.d64
 find fluxvis -name \*.py | while read l; do c1541 -attach disk.d64 -write "$l" "`echo ${l##*/} | tr A-Z a-z`"; done
 
 ~/src/fluxengine/fluxengine write commodore1541 --no-verify -d disk.scp -i disk.d64
-PYTHONPATH=../greaseweazle/scripts/ python3 -mfluxvis disk.scp etc/disk.jpg --oversample 3 --resolution 640 --stride 2 --tracks 35 --diameter 108
+python3 -mfluxvis disk.scp etc/disk.jpg --oversample 3 --resolution 640 --stride 2 --tracks 35 --diameter 108
