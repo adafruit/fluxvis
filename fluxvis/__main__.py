ad9e2e06cd05fd86c38bdca21ca5579f5f91b800
Jeff Epler <jepler@gmail.com>
2022-04-26 08:30:03 -0500

    Factor out render_flux

--- fluxvis/__main__.py
+++ fluxvis/__main__.py
@@ -4,26 +4,11 @@
 
 """Flux visualizer"""
 
-import csv
 import numpy as np
 import click
 from skimage.transform import downscale_local_mean
 from skimage.io import imsave
-from skimage.color import gray2rgb
-from . import invpolar
-from . import open_flux
-
-PHYSICAL_TRACK = "Physical track"
-PHYSICAL_SIDE = "Physical side"
-HEADER_START_NS = "Header start (ns)"
-HEADER_END_NS = "Header end (ns)"
-DATA_START_NS = "Data start (ns)"
-DATA_END_NS = "Data end (ns)"
-STATUS = "Status"
-
-HEADER_COLOR = np.array((128, 128, 255), dtype=np.uint8)
-DATA_OK_COLOR = np.array((128, 255, 128), dtype=np.uint8)
-DATA_BAD_COLOR = np.array((255, 128, 128), dtype=np.uint8)
+from . import invpolar, open_flux, render_flux
 
 
 @click.command()
@@ -84,91 +69,10 @@ def main(
     else:
         major = round(diameter * stacks / 2)
 
-    density = np.zeros((major, slices), dtype=np.float32)
-    r = np.arange(slices)
+    density = render_flux(
+        flux, side, tracks, start, stride, major, slices, stacks, location
+    )
 
-    track_duration = {}
-
-    for cyl in range(0, tracks):
-        t0 = major - (1 + cyl) * stacks
-        t1 = major - cyl * stacks - 1
-        track = flux.get_track(cyl * stride + start, side)
-        if track is None:
-            continue
-        track.cue_at_index()
-        if not track.index_list:
-            index = sum(track.list)
-        else:
-            index = track.index_list[0]
-        index = track.index_list[0]
-        track_duration[cyl] = index
-        indices = np.array(track.list, dtype=np.float32) * ((slices - 1) / index)
-        indices = np.floor(np.add.accumulate(indices))
-        lo = np.searchsorted(indices, r, "left")
-        hi = np.searchsorted(indices, r, "right")
-        density[t0:t1, :] = hi - lo
-
-    def ns2loc(offset_ns, cyl_duration):
-        return round(
-            offset_ns * flux.sample_freq / 1_000_000_000 / cyl_duration * (slices - 1)
-        )
-
-    if location is not None:
-
-        def paint(t0, t1, start, end, color):
-            while start > slices:
-                start -= slices
-                end -= slices
-            status[t0 : t1 + 1, start:end] = color
-            if end > slices:
-                status[t0 : t1 + 1, : end % slices] = color
-
-        status = np.zeros((major, slices, 3), dtype=np.uint8) + 255
-        with open(location, "r", encoding="utf-8", errors="ignore") as f:
-            for row in csv.DictReader(f):
-                physical_track = row.get(PHYSICAL_TRACK)
-                physical_side = row.get(PHYSICAL_SIDE)
-                header_start = row.get(HEADER_START_NS)
-                header_end = row.get(HEADER_END_NS)
-                data_start = row.get(DATA_START_NS)
-                data_end = row.get(DATA_END_NS)
-                data_status = row.get(STATUS)
-
-                if any(x is None for x in (physical_track, physical_side, data_status)):
-                    continue
-
-                physical_side = int(physical_side)
-                physical_track = int(physical_track)
-                if physical_side != side:
-                    continue
-                if physical_track % stride != start:
-                    continue
-
-                cyl = (physical_track - start) // stride
-                if cyl not in track_duration:
-                    continue
-                t0 = major - (1 + cyl) * stacks
-                t1 = major - cyl * stacks - 1
-
-                if header_start is not None and header_end is not None:
-                    header_start = ns2loc(float(header_start), track_duration[cyl])
-                    header_end = ns2loc(float(header_end), track_duration[cyl])
-                    if data_start is not None:  # artifically enlarge header mark
-                        header_end = ns2loc(float(data_start), track_duration[cyl])
-                    paint(t0, t1, header_start, header_end, HEADER_COLOR)
-
-                if data_start is not None and data_end is not None:
-                    data_start = ns2loc(float(data_start), track_duration[cyl])
-                    data_end = ns2loc(float(data_end), track_duration[cyl])
-                    paint(
-                        t0,
-                        t1,
-                        data_start,
-                        data_end,
-                        DATA_OK_COLOR if data_status == "OK" else DATA_BAD_COLOR,
-                    )
-
-        density = gray2rgb(density) * status
     if not linear:
         multichannel = len(density.shape) == 3
         density = invpolar.warp_inverse_polar(
